62de6420571d0fc0b030c78e927a8fd36e083952
DenKu <Denkuzia5155@gmail.com>
2024-12-21 18:36:55 +0300

    #1

---
+++ src/main/java/com/example/kata_preprojekt/KataPreProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.kata_preprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataPreProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataPreProjektApplication.class, args);
+	}
+
+}
